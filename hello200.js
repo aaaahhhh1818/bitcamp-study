d098e9a6228c5ce0fcc874963ab482676bf52cf7
aaaahhhh1818 <aaaaahhhhh1818@hanmail.net>
2021-06-09 16:44:33 +0900

    테스트테스트2

--- hello200.js
+++ hello200.js
@@ -1,5 +1,8 @@
 console.log("안녕하세요. 이승철입니다.");
+console.log("안녕하세요. 이아현입니다.");
 console.log("안녕하세요. 이승철입니다.");
+console.log("안녕하세요. 이아현입니다.");
 console.log("안녕하세요. 이승철입니다.");
+console.log("안녕하세요. 이아현입니다.");
 console.log("안녕하세요. 이승철입니다.");
-console.log("안녕하세요. 이승철입니다.");
+console.log("안녕하세요. 이아현입니다.");
--- hello205.js
+++ hello205.js
@@ -1,3 +1,4 @@
 console.log("hello205");
+console.log("헬로이백오");
 console.log("hello205");
-console.log("hello205");
+console.log("하이이백오");
