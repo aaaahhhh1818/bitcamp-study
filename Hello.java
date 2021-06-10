15da3b37b24ec0e5da028d641d2b6a32d2d7f366
aaaahhhh1818 <aaaaahhhhh1818@hanmail.net>
2021-06-10 19:34:25 +0900

    20210610

--- Hello.java
+++
@@ -1,5 +0,0 @@
-public class Hello {
-        public static void main(String[] arge) {
-            System.out.println("Hello, world!");
-        }
-}
---
+++ java-basic/app/src/main/java/Hello3.java
@@ -0,0 +1,4 @@
+class Hello3 {
+        public static void main(String[] arge) {
+                //기본 문법 
+        }
