9ce2781b2122b55e631909e77f5685cd228b451c
aaaahhhh1818 <aaaaahhhhh1818@hanmail.net>
2021-07-05 17:34:49 +0900

    20210705

--- Mtest/insa.sql
+++ Mtest/insa.sql
@@ -1,13 +1,14 @@
 drop table insa;
 
-create table insa (
-  sabun number (4) not null primary key ,
-  name varchar (10) not null ,
-  buse varchar (20) not null
+create table board(
+  code number(4) not null primary key ,
+  write varchar(10) not null ,
+  title varchar(20) not null ,
+  content varchar(60) not null ,
+  email varchar(20) not null ,
+  cnt number(3) not null ,
+  wdate date not null ,
+  img varchar(30) not null ,
+  pwd varchar(20) not null ,
+  sns varchar(30) not null 
 );
-
-commit;
-desc insa
-insert into insa(sabun, name, buse) values ( 990, 'dan', 'gugu' );
-commit;
-select sabun, name, buse from insa;
