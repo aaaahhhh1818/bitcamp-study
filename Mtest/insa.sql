5ba52cbfeb261ccf738d54af09b19277de2972fd
aaaahhhh1818 <aaaaahhhhh1818@hanmail.net>
2021-07-06 17:29:57 +0900

    20210706

--- Mtest/insa.sql
+++ Mtest/insa.sql
@@ -1,14 +1,18 @@
 drop table insa;
 
-create table board(
-  code number(4) not null primary key ,
-  write varchar(10) not null ,
-  title varchar(20) not null ,
-  content varchar(60) not null ,
-  email varchar(20) not null ,
-  cnt number(3) not null ,
-  wdate date not null ,
-  img varchar(30) not null ,
-  pwd varchar(20) not null ,
-  sns varchar(30) not null 
-);
+create table insa (
+  sabun number(4) not null  primary key , 
+      name  varchar(10) not null ,
+      buse  varchar(20) not null  
+ );
+
+commit;
+
+insert into insa(sabun,name,buse) values( 1100, 'aaa', 'ggg');
+insert into insa(sabun,name,buse) values( 2200, 'bbb', 'xxxx');
+insert into insa(sabun,name,buse) values( 3300, 'ccc', 'vvvv');
+insert into insa(sabun,name,buse) values( 4400, 'ddd', 'bbbbb');
+insert into insa(sabun,name,buse) values( 5500, 'eee', 'wwww');
+
+commit;
+select * from insa;
