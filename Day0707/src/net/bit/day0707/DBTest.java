c91dff17a6b9d8a71ff54d30ae180e62a0cc58f0
aaaahhhh1818 <aaaaahhhhh1818@hanmail.net>
2021-07-07 17:45:55 +0900

    20210707

---
+++ Day0707/src/net/bit/day0707/DBTest.java
@@ -0,0 +1,14 @@
+package net.bit.day0707;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class DBTest {
+
+  public static void main(String[] args) {
+    Connection CN = null;
+    Statement ST = null;
+
+  }
+
+}
